da70bb6d7d65541389a2fd4406672dc4131e8353
Rasmus Bækgaard <git@bakgaard.net>
2014-03-05 10:56:13 +0100

    Add File to write Meggit code in and helper function TriAnd

---
+++ Meggitt/MeggittTest.m
@@ -0,0 +1,22 @@
+clc, clear
+
+%%Meggitt decoder
+
+%g(x) = 1 + x + x^3
+g = [1 1 0 1];
+
+%v(x)
+v = [1 0 0 1 0 1 1];
+
+r = [1 0 1 1 0 1 1];
+
+%Shift til højre
+rSh = circshift(r,[-1 1]);
+
+n = 7;
+k = 4;
+
+
+[parmat,genmat,h] = cyclgen(n,g,'system');
+
+syndrome = mod(rSh*parmat',2)
---
+++ Meggitt/TriAnd.m
@@ -0,0 +1,9 @@
+function res = TriAnd(a, b, c)
+
+if( a == 1 && b == 0 && c == 1)
+    res = 1;
+else
+    res = 0;
+end
+
+end
