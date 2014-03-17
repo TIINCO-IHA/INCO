4082a107923a6121bf5064ea850cbda86ea35933
Rasmus Bækgaard <git@bakgaard.net>
2014-03-17 10:23:50 +0100

    Add index number to all sections and subsections, add comments in .m files, add Meggitt description

--- Meggitt/TriAnd.m
+++ Meggitt/TriAnd.m
@@ -1,9 +1,10 @@
 function res = TriAnd(a, b, c)
+%And gate with second input inverted
+%Only input [1 0 1] will return 1
 
-if( a == 1 && b == 0 && c == 1)
-    res = 1;
-else
-    res = 0;
-end
-
+    if( a == 1 && b == 0 && c == 1)
+        res = 1;
+    else
+        res = 0;
+    end
 end
