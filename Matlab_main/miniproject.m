5b1f7530a4b8f91fda6012ca1c57ea343e7c2f2b
Janne <janne.walsted@hotmail.com>
2014-03-20 19:11:18 +0100

    Opdateret main function implementation+kommentarer i miniproject.m-kode

--- Matlab_main/miniproject.m
+++ Matlab_main/miniproject.m
@@ -21,10 +21,11 @@ m = mod(randi(2,1,k),2);
 c = cyclicEncoding(g,m,n,k);
 
 % Transmit the code vector through an artificial communication channel,
-% i.e. introduce one or two errors to the code vector:
+% i.e. introduce at least one error to the code vector:
 
-% If the number of errors stated to be introduced is zero, introduce 2 errors. 
-% If no error location has been stated, generate random error locations and state them in a sorted array
+% If the number of errors stated to be introduced is zero, introduce 2 errors
+% If no error location has been stated, generate random error locations
+% State the error locations in a sorted array
 if(nargin < 1)  % no input parameter stating number of errors
     t = 2;
     errorlocation = sort(randi(n,1,t));
@@ -37,32 +38,36 @@ elseif(nargin > 0)
     end
 end
 
+% If there are two or more errors and they happen to be in the same 
+% location then generate two new error locations
 while(t > 2 && length(unique(errorlocation)) ~= t)
     errorlocation = sort(randi(n,1,t));
     disp('Errorlocation was recalculated!');
 end
+
+% Put the code vector c into the received vector r
 r = c;
 
-% Flip bit at generated error location i the received vector.
-% Add 1 and modulo 2 for binary addition
+% Introduce errors at the generated error location(s) i the received vector
+% by adding 1 and modulo 2 for binary addition (flipping the bits)
 r(errorlocation) = mod(c(errorlocation)+1,2);
 
-% Decode the received vector by the meggitt decoder
+% Decode the received vector by the Meggitt decoder
 [e, r_c, tag] = meggitt_decoder(r, g, n, k);
 
+% Print whether the decoding was successfull 
 if(isequal(c, r_c) == 0)
     disp('The codeword was not decoded correct');
 else
     disp('The codeword was successfully decoded');
 end
-
-c_t = mat2str(c);
-e_t = mat2str(e);
-r_t = mat2str(r);
-rc_t = mat2str(r_c);
+c_t = mat2str(c);     % code vector
+r_t = mat2str(r);     % received vector
+e_t = mat2str(e);     % error vector
+rc_t = mat2str(r_c);  % corrected vector
 t_out = 'The coded vector is    c = %s.\nAfter transmission is  r = %s.\nThe error vector is    e = %s.\nThe corrected vector r_c = %s\nThe tag from the decoder claims: %s.\n';
 fprintf(t_out,c_t,r_t,e_t,rc_t,tag);
-% clear c_t; clear e_t; clear r_t; clear rc_t; clear t_out;
 
+% Verify that the result of the decoding is equal to the code vector
 save variables
 test = isequal(c, r_c);
